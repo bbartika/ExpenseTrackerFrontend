6ed4680418b81d8f97127c04fc3437ce476100dc
bbartika <137605070+bbartika@users.noreply.github.com>
2023-11-12 11:10:02 +0530

    Update login.js

--- Login/login.js
+++ Login/login.js
@@ -11,12 +11,29 @@ function login(e) {
     axios.post('http://localhost:3000/user/login',loginDetails).then(response => {
             alert(response.data.message)
             console.log(response.data)
-            
+            localStorage.setItem('token', response.data.token)
+            window.location.href = "../ExpenseTracker/index.html"
     }).catch(err => {
         console.log(JSON.stringify(err))
         document.body.innerHTML += `<div style="color:red;">${err.message} <div>`;
     })
 }
+eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY2MjcwNTE3MX0.wDC7ABZuczLS4Zqv8Yzquqi2bGwX5J_pWqamP7gXS4I
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

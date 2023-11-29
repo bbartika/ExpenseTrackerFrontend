107c5102bdd1af4efebb5c0369e299600e467e85
bbartika <137605070+bbartika@users.noreply.github.com>
2023-11-29 22:26:49 +0530

    Update login.js

--- Login/login.js
+++ Login/login.js
@@ -1,27 +1,30 @@
 function login(e) {
     e.preventDefault();
     console.log(e.target.name);
+    const form = new FormData(e.target);
 
     const loginDetails = {
-        email: e.target.email.value,
-        password: e.target.password.value
+        email: form.get("email"),
+        password: form.get("password")
 
     }
     console.log(loginDetails)
     axios.post('http://localhost:3000/user/login',loginDetails).then(response => {
-            alert(response.data.message)
-            console.log(response.data)
-            localStorage.setItem('token', response.data.token)
-            window.location.href = "../ExpenseTracker/index.html"
+        if(response.status === 200){
+            localStorage.setItem('token', response.data.token);
+            localStorage.setItem('userDetails', JSON.stringify(response.data.user))
+            window.location.href = "../ExpenseTracker/index.html" // change the page on successful login
+        } else {
+            throw new Error('Failed to login')
+        }
     }).catch(err => {
-        console.log(JSON.stringify(err))
-        document.body.innerHTML += `<div style="color:red;">${err.message} <div>`;
+        document.body.innerHTML += `<div style="color:red;">${err} <div>`;
     })
 }
-eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY2MjcwNTE3MX0.wDC7ABZuczLS4Zqv8Yzquqi2bGwX5J_pWqamP7gXS4I
 
-
-
+function forgotpassword() {
+    window.location.href = "../ForgotPassword/index.html"
+}
 
 
 
